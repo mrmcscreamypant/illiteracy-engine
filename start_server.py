aa0fad6917b6b4d16222011f589f4b3651a6f19b
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2024-06-26 05:11:48 +0000

    Sick new loading screen

--- start_server.py
+++ start_server.py
@@ -1,5 +1,6 @@
-from bottle import *
+from bottle import route, run
 from config.build import CLIENT_FILE_PATH
+from config.webserver import PATH_TO_GAME
 
 print("\nIlliteracy Engine - v0.0.0alpha\n")
 
@@ -10,11 +11,21 @@ def get_client_file():
     print("client file fetched sucessfully")
   return data
 
-client_file = get_client_file()
+def get_bootstrap_client_file():
+  path_to_file = "webserver/static/bootstrap_client.html"
+  
+  print(f"fetching bootstrap client file '{path_to_file}'")
+  with open(path_to_file,"r") as file:
+    data = file.read()
+    print("client bootstrap file fetched sucessfully")
+  return data
 
-@route("/")
+client_file = get_client_file()
+bootstrap_client_file = get_bootstrap_client_file()
+
+@route(PATH_TO_GAME)
 def game():
-  return client_file
+  return bootstrap_client_file
 
 print("\nLaunching bottle server...\n")
 
